54650333cc06aa5265bc7f8309b366f00ade0d43
Asar Ahmed (Full Stack devleoper) <themernist@gmail.com>
2024-01-24 00:16:38 +0500

    intial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn_division = document.querySelector("#division");
+const btn_multiply = document.querySelector("#multiply");
+const btn_minus = document.querySelector("#minus");
+const btn_clear = document.querySelector("#clear");
+const btn_7 = document.querySelector("#btn_7");
+const btn_8 = document.querySelector("#btn_8");
+const btn_9 = document.querySelector("#btn_9");
+const btn_4 = document.querySelector("#btn_4");
+const btn_5 = document.querySelector("#btn_5");
+const btn_6 = document.querySelector("#btn_6");
+const btn_dot = document.querySelector("#btn_dot");
+const btn_1 = document.querySelector("#btn_1");
+const btn_2 = document.querySelector("#btn_2");
+const btn_3 = document.querySelector("#btn_3");
+const btn_ans = document.querySelector("#btn_ans");
+const btn_zero = document.querySelector("#btn_zero");
+const display = document.querySelector(".display span");
+const btn_addision = document.querySelector("#btn_addision");
+
+const  x = eval(55+5)
+console.dir(x)
